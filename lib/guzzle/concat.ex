d5c67580a8d2235f9ee7b61ddf4d6f1d412c940d
Josh Nussbaum <joshnuss@gmail.com>
2016-10-25 22:54:27 -0400

    Refactor concat

--- lib/guzzle/concat.ex
+++ lib/guzzle/concat.ex
@@ -1,10 +1,10 @@
 defmodule Guzzle.Concat do
   def concat(flow, path \\ "all"),
-    do: [{path, concat_data(flow)}]
+    do: [{path, join(flow)}]
 
-  defp concat_data(flow),
+  defp join(flow),
     do: Enum.map_join(flow, &extract/1)
 
-  defp extract({path, data}),
+  defp extract({_path, data}),
     do: data
 end
