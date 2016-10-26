9c3290c59392c0e8b59351a78c9e9dc3e55c7850
Josh Nussbaum <joshnuss@gmail.com>
2016-10-25 22:37:18 -0400

    Renamed test/examples to test/fixtures

--- test/source_test.exs
+++ test/source_test.exs
@@ -5,22 +5,22 @@ defmodule SourceTest do
   import Guzzle.Source
 
   test "single glob" do
-    files = source("test/examples/*.html")
+    files = source("test/fixtures/*.html")
       |> Enum.to_list
 
-    assert files == [{"test/examples/about.html", "<h1>About</h1>\n"},
-                     {"test/examples/index.html", "<h1>Index</h1>\n"}]
+    assert files == [{"test/fixtures/about.html", "<h1>About</h1>\n"},
+                     {"test/fixtures/index.html", "<h1>Index</h1>\n"}]
   end
 
   test "multiple globs" do
-    files = source(["test/examples/a*.html", "test/examples/i*.htm?"])
+    files = source(["test/fixtures/a*.html", "test/fixtures/i*.htm?"])
       |> Enum.to_list
 
-    assert files == [{"test/examples/about.html", "<h1>About</h1>\n"},
-                     {"test/examples/index.html", "<h1>Index</h1>\n"}]
+    assert files == [{"test/fixtures/about.html", "<h1>About</h1>\n"},
+                     {"test/fixtures/index.html", "<h1>Index</h1>\n"}]
   end
   test "search returns empty" do
-    files = source("test/examples/*.noexist")
+    files = source("test/fixtures/*.noexist")
       |> Enum.to_list
 
     assert files == []
