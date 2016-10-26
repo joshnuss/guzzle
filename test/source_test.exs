87437d2f4b9918c618898a2a9a401b496a806510
Josh Nussbaum <joshnuss@gmail.com>
2016-10-25 22:54:33 -0400

    Added missing line

--- test/source_test.exs
+++ test/source_test.exs
@@ -19,6 +19,7 @@ defmodule SourceTest do
     assert files == [{"test/fixtures/about.html", "<h1>About</h1>\n"},
                      {"test/fixtures/index.html", "<h1>Index</h1>\n"}]
   end
+
   test "search returns empty" do
     files = source("test/fixtures/*.noexist")
       |> Enum.to_list
