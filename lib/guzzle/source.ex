6886ee9869b5aa717ba6bcf8e2b8e836029adc92
Josh Nussbaum <joshnuss@gmail.com>
2016-10-28 22:29:13 -0400

    Removed unused __using__ clause

--- lib/guzzle/source.ex
+++ lib/guzzle/source.ex
@@ -1,12 +1,6 @@
 defmodule Guzzle.Source do
   alias Experimental.Flow
 
-  defmacro __using__(_opts) do
-    quote do
-      alias Guzzle.Source
-    end
-  end
-
   def source(globs) do
     List.wrap(globs)
     |> Flow.from_enumerable
